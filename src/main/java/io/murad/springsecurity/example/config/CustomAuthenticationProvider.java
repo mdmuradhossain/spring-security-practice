3c78e41878919ccda50c2df8f156c656d7576f10
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-23 20:00:01 +0600

    CustomAuthenticationProvider added

--- src/main/java/io/murad/springsecurity/example/config/CustomAuthenticationProvider.java
+++ src/main/java/io/murad/springsecurity/example/config/CustomAuthenticationProvider.java
@@ -2,35 +2,65 @@ package io.murad.springsecurity.example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
 
 @Configuration
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
+//    @Override
+//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+//        String username = authentication.getName();
+//        String password = String.valueOf(authentication.getCredentials());
+//
+//        if ("murad".equals(username) && "admin".equals(password)) {
+//            return new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList());
+//        } else {
+//            throw new AuthenticationCredentialsNotFoundException("Error!");
+//        }
+//    }
+//
+//    @Override
+//    public boolean supports(Class<?> authenticationType) {
+//        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authenticationType);
+//    }
+
+    //Custom implementation of AuthenticationProvider
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
-        String password = String.valueOf(authentication.getCredentials());
+        String password = authentication.getCredentials().toString();
 
-        if ("murad".equals(username) && "admin".equals(password)) {
-            return new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList());
+        UserDetails u = userDetailsService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, u.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    u.getAuthorities()
+            );
         } else {
-            throw new AuthenticationCredentialsNotFoundException("Error!");
+            throw new BadCredentialsException("Something went wrong!");
         }
     }
 
     @Override
     public boolean supports(Class<?> authenticationType) {
-        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authenticationType);
+        return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
