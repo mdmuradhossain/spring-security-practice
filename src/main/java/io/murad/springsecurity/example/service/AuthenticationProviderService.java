783df86f2102f5d55c426730992f8caec9949ac9
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-08-27 23:01:09 +0600

    jwt implementing...

--- src/main/java/io/murad/springsecurity/example/service/AuthenticationProviderService.java
+++ src/main/java/io/murad/springsecurity/example/service/AuthenticationProviderService.java
@@ -1,68 +1,68 @@
-//package io.murad.springsecurity.example.service;
-//
-//import io.murad.modern.ecommerce.database.model.CustomUserDetails;
-//import lombok.AllArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.BadCredentialsException;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class AuthenticationProviderService implements AuthenticationProvider {
-//
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//    @Autowired
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
-//    @Autowired
-//    private SCryptPasswordEncoder sCryptPasswordEncoder;
-//
-//    @Override
-//    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-//        String username = authentication.getName();
-//        String password = authentication.getCredentials().toString();
-//
-//        UserDetails user =  userDetailsService.loadUserByUsername(username);
-//
+package io.murad.springsecurity.example.service;
+
+import io.murad.modern.ecommerce.database.model.CustomUserDetails;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationProviderService implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private SCryptPasswordEncoder sCryptPasswordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails user =  userDetailsService.loadUserByUsername(username);
+
 //        switch (user.getUser().getAlgorithm()) {
 //            case BCRYPT:
 //                return checkPassword(user, password, bCryptPasswordEncoder);
 //            case SCRYPT:
 //                return checkPassword(user, password, sCryptPasswordEncoder);
 //        }
-//        throw new BadCredentialsException("Bad credentials");
-//
-//    }
-//
-//    @Override
-//    public boolean supports(Class<?> authenticationType) {
-//        return UsernamePasswordAuthenticationToken.class
-//                .isAssignableFrom(authenticationType);
-//    }
-//
-//    private Authentication checkPassword(UserDetails user,
-//                                         String rawPassword,
-//                                         PasswordEncoder encoder) {
-//        if (encoder.matches(rawPassword, user.getPassword())) {
-//            return new UsernamePasswordAuthenticationToken(
-//                    user.getUsername(),
-//                    user.getPassword(),
-//                    user.getAuthorities());
-//
-//        } else {
-//            throw new BadCredentialsException("Bad credentials!");
-//        }
-//
-//
-//    }
-//
-//}
+        throw new BadCredentialsException("Bad credentials");
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authenticationType) {
+        return UsernamePasswordAuthenticationToken.class
+                .isAssignableFrom(authenticationType);
+    }
+
+    private Authentication checkPassword(UserDetails user,
+                                         String rawPassword,
+                                         PasswordEncoder encoder) {
+        if (encoder.matches(rawPassword, user.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getAuthorities());
+
+        } else {
+            throw new BadCredentialsException("Bad credentials!");
+        }
+
+
+    }
+
+}
